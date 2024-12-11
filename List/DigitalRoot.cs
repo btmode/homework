9e826bb5007ba9753b3056a01f7a174181f68d28
artur <example@a.b>
2024-12-11 14:18:09 +0500

    DigitalRoot комментарии

--- List/DigitalRoot.cs
+++ List/DigitalRoot.cs
@@ -2,14 +2,18 @@
 
 class DigitalRoot
 {
+    // По именованию метода больше похоже, что он должен возвращать bool.
+    // То есть ты назвал Has - имеет или не имеет. А метод находит цифровой корень числа, поэтому нужно его назвать Get или Find
     public static int HasRoot(int number)
     {
+        // Обработать ситуацию, когда число отрицательное, например -123
         if (number < 10)
             return number;
 
         return HasRoot(IsRoot(number));
     }
 
+    // Тут в целом не стоило разбивать на два отдельных метода, можно сделать все в одном
     public static int IsRoot(int digit)
     {
         var number = 0;
